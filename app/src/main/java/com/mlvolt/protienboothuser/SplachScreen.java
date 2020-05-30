353e1e7c32c1fb26435e104fa8df01516ce44dce
diipak-singh <deepaksinghbaise@gmail.com>
2020-05-30 19:47:31 +0530

    fixes authentication issues

--- app/src/main/java/com/mlvolt/protienboothuser/SplachScreen.java
+++ app/src/main/java/com/mlvolt/protienboothuser/SplachScreen.java
@@ -1,44 +1,95 @@
 package com.mlvolt.protienboothuser;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplachScreen extends AppCompatActivity {
+    private static final int MY_REQUEST_CODE = 007;
+    List<AuthUI.IdpConfig> providers;
+    private FirebaseUser user;
 
     private int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //requesting full-screen window
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splach_screen);
+        //getting current user if it is logged in.
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //providing phone-builder into provider's array.
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build()
+        );
 
         new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                // Start your app main activity
-                FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
-                if(current_user!=null){
-                    Intent intent1 = new Intent(SplachScreen.this, MainActivity.class);
-                    SplachScreen.this.startActivity(intent1);
-                    SplachScreen.this.finish();
+                // Do whatever u want.
 
-                }else{
-
-                    Intent intent2 = new Intent(SplachScreen.this, Login.class);
-                    SplachScreen.this.startActivity(intent2);
-                    SplachScreen.this.finish();
-
+                if (user != null) {
+                    Intent intent = new Intent(SplachScreen.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                } else {
+                    showSignInOptions();
                 }
+
             }
         }, SPLASH_TIME_OUT);
 
     }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.AppTheme)
+                        .build(), MY_REQUEST_CODE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE) {
+
+            /*// Checking Response.
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            Log.i("Response",response.toString());*/
+
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Successfully LoggedIn", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SplachScreen.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }
+    }
+
 
 }
